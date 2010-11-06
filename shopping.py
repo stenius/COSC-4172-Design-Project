e632fa20cf5983cbe895172f738242eda0c4dfef
Paul Griffith Stenius <pauls@aqm.lamar.edu>
2010-11-06 13:19:42 -0500

    rewrote the method of allowing customers that were disatisfied to rejoin

--- shopping.py
+++ shopping.py
@@ -25,21 +25,42 @@ class Source(Process):
             activate(c, c.visit(res=resource,P=0))
             t = expovariate(1.0/interval)
             yield hold,self,t
-
+def probWillHappen(probability):
+    rand = random.random()
+    if rand < probability:
+        return True
+    return False
 class Customer(Process):
-    def visit(self, timeBeingServed=1, res=None, P=0):
-        timeBeingServed = uniform(2/(P+1),2.8/(P+1))
+    def visit(self, timeBeingServed=1, res=None, P=0, satisfied=False):
+        """P plays double duty as both the probability and the priority"""
+        timeBeingServed = uniform(2./(P+1),2.8/(P+1))
         arrive = now()
         Nwaiting = len(res.waitQ)
-        print "%8.3f %s: Queue is %d on arrival"%(now(),self.name,Nwaiting)
+        print "%8.3f %s(%1i): Queue has %d customers in line on arrival"%(now(),self.name,P,Nwaiting)
 
         yield request,self,res,P
         wait = now() - arrive #waiting time
-        print "%8.3f %s: Waited %6.3f"%(now(),self.name,wait)
+        print "%8.3f %s: Waited %6.3f in line"%(now(),self.name,wait)
         yield hold,self,timeBeingServed
         yield release,self,res
+        while not satisfied:
+            if probWillHappen(.2/(P+1)):
+                #make the customer rejoin the queue
+                print "%8.3f %s: Customer Displeased "%(now(),self.name)
+                P = P+1
+                arrive = now()
+                yield request,self,res,P
+                timeBeingServed = uniform(2./(P+1),2.8/(P+1))
+
+                wait = now() - arrive #waiting time
+                print "%8.3f %s: Waited %6.3f in line"%(now(),self.name,wait)
+                yield hold,self,timeBeingServed
+                yield release,self,res
+
+            else:
+                satisfied=True
+
         if not random.randint(0,4):
-            print "%8.3f %s: Customer Displeased"%(now(),self.name)
             #rejoin the queue
             yield request,self,res,P+1
         print "%8.3f %s: Completed"%(now(),self.name)
@@ -51,9 +72,9 @@ def main():
     seed(99999)
     initialize()
     s = Source('Source')
+
+    #make a boatload of customers
     activate(s,s.generate(numberOfCustomers=500, resource=shop), at=0.0)
-    guido = Customer(name='Guido')
-    activate(guido,guido.visit(timeBeingServed=12.0, res=shop,P=100), at=23.0)
     simulate(until=maxTime)
 
 
