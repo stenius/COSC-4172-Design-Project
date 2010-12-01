07486696a54a6db28e109ced852692a04fbfceb9
Paul Griffith Stenius <stenius@gmail.com>
2010-12-01 16:11:29 -0600

    fixed early terminal logical error and added seed cycling
    also added verbose flag to supress output only useful for debugging or advanced analysis

--- shopping.py
+++ shopping.py
@@ -16,6 +16,7 @@ from SimPy.Simulation import *
 
 from random import expovariate, seed
 
+verbose = False
 #model components
     
 waitMonitor = Monitor()
@@ -36,60 +37,82 @@ def probWillHappen(probability):
 class Customer(Process):
     def visit(self, timeBeingServed=1, res=None, P=0, satisfied=False):
         """P plays double duty as both the probability and the priority"""
-        timeBeingServed = uniform(2./(P+1),2.8/(P+1))
+        timeBeingServed = uniform(2./(P+1.),2.8/(P+1.))
         arrive = now()
         Nwaiting = len(res.waitQ)
         queueLengthMonitor.observe(Nwaiting)
-        print "%8.3f %s(%1i): Queue has %d customers in line on arrival"%(now(),self.name,P,Nwaiting)
+        if verbose:
+            print "%8.3f %s(%1i): Queue has %d customers in line on arrival"%(now(),self.name,P,Nwaiting)
 
         yield request,self,res,P
         wait = now() - arrive #waiting time
         waitMonitor.observe(wait)
-        print "%8.3f %s: Waited %6.3f in line"%(now(),self.name,wait)
+        if verbose:
+            print "%8.3f %s: Waited %6.3f in line"%(now(),self.name,wait)
         yield hold,self,timeBeingServed
         serverTimeMonitor.observe(timeBeingServed)
-        print "%8.3f %s(%1i): waited on the cashier for %8.3f minutes"%(now(),self.name,P,timeBeingServed)
+        if verbose:
+            print "%8.3f %s(%1i): cashier took %8.3f minutes to check out items."%(now(),self.name,P,timeBeingServed)
         yield release,self,res
         while not satisfied:
             if probWillHappen(.2/(P+1)):
                 #make the customer rejoin the queue
-                print "%8.3f %s: Customer Displeased "%(now(),self.name)
+                if verbose:
+                    print "%8.3f %s: Customer Displeased "%(now(),self.name)
                 P = P+1
                 arrive = now()
                 yield request,self,res,P
-                timeBeingServed = uniform(2./(P+1),2.8/(P+1))
+                timeBeingServed = uniform(2./(P+1.),2.8/(P+1.))
 
                 wait = now() - arrive #waiting time
-                print "%8.3f %s: Waited %6.3f in line"%(now(),self.name,wait)
+                if verbose:
+                    print "%8.3f %s: Waited %6.3f in line"%(now(),self.name,wait)
                 yield hold,self,timeBeingServed
-                print "%8.3f %s(%1i): waited on the cashier for %8.3f minutes, again"%(now(),self.name,P,timeBeingServed)
+                if verbose:
+                    print "%8.3f %s(%1i): waited on the cashier for %8.3f minutes, again"%(now(),self.name,P,timeBeingServed)
                 yield release,self,res
-
             else:
                 satisfied=True
 
-        if not random.randint(0,4):
+#        if not random.randint(0,4):#1/5 chance of rejoing queue
             #rejoin the queue
-            yield request,self,res,P+1
-        print "%8.3f %s: Completed"%(now(),self.name)
+#            yield request,self,res,P+1
+        if verbose:
+            print "%8.3f %s: Completed"%(now(),self.name)
 
 #simulation data
 maxTime = 480. #minutes
 def main():
-    shop = Resource(capacity=5, name='Shop', unitName="Lane", qType=PriorityQ)
-    seed(99999)
-    initialize()
-    s = Source('Source')
-    #make a boatload of customers
-    activate(s,s.generate(numberOfCustomers=500, resource=shop), at=0.0)
-    simulate(until=maxTime)
-
-    result = waitMonitor.count(),waitMonitor.mean()                             
-    print "Average wait in queue was for %3d completions was %5.3f minutes."% result
-    result = queueLengthMonitor.count(),queueLengthMonitor.mean()                             
-    print "Average queue length for %3d completions was %5.3f cusomters."% result
-    result = serverTimeMonitor.count(),serverTimeMonitor.mean()                             
-    print "Average time server took in lane for %3d completions was %5.3f \
-    minutes."% result
+    seeds = [
+        99999,
+        99998,
+        99997,
+        99996,
+        99995,
+        99994,
+        99993,
+        99992,
+        99991,
+        99990,]
+    for seedVal in seeds:
+        print 'using seed value ' + str(seedVal) + ':'
+        shop = Resource(capacity=5, name='Shop', unitName="Lane", qType=PriorityQ)
+        seed(seedVal)
+        initialize()
+        waitMonitor.reset()
+        queueLengthMonitor.reset()
+        serverTimeMonitor.reset()
+        s = Source('Source')
+        #make a boatload of customers
+        activate(s,s.generate(numberOfCustomers=500, resource=shop), at=0.0)
+        simulate(until=maxTime)
+        result = waitMonitor.count(),waitMonitor.mean()                             
+        print "\tAverage wait in queue was for %3d completions was %5.3f minutes."% result
+        result = queueLengthMonitor.count(),queueLengthMonitor.mean()                             
+        print "\tAverage queue length for %3d completions was %5.3f cusomters."% result
+        result = serverTimeMonitor.count(),serverTimeMonitor.mean()                             
+        print "\tAverage time server took in lane for %3d completions was %5.3f \
+        minutes."% result
+        stopSimulation()
 if __name__ == "__main__":
     main()
